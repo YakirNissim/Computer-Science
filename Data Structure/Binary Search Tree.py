358632ed083b86ab13aa2e50eebae31fb417806f
Yakir Nissim <yakir.31.05@gmail.com>
2022-08-23 10:51:53 +0300

    update Binary Search Tree.py: add print_tree and get_children functions

--- Data Structure/Binary Search Tree.py
+++ Data Structure/Binary Search Tree.py
@@ -1,35 +1,3 @@
-class Node (object):
-    def __init__(self, data, father=None, left_child=None, right_child=None):
-        self.data = data
-        self.father = father
-        self.left_child = left_child
-        self.right_child = right_child
-
-    def get_data(self):
-        return self.data
-
-    def get_left_child(self):
-        return self.left_child
-
-    def get_father(self):
-        return self.father
-
-    def get_right_child(self):
-        return self.right_child
-
-    def set_data(self, new_data):
-        self.data = new_data
-
-    def set_father(self, new_father):
-        self.father = new_father
-
-    def set_left_child(self, new_left_child):
-        self.left_child = new_left_child
-
-    def set_right_child(self, new_right_child):
-        self.right_child = new_right_child
-
-
 class Binary_Search_Tree (object):
     def __init__(self, root=None):
         self.root = root
@@ -66,7 +34,7 @@ class Binary_Search_Tree (object):
         elif check_node is None:
             check_node = self.root
 
-        print(check_node.get_data())
+        # print(check_node.get_data())
         if data == check_node.get_data():
             return True, check_node
         elif data < check_node.get_data():
@@ -80,7 +48,7 @@ class Binary_Search_Tree (object):
             else:
                 return False, None
 
-    def remove(self, data, node=None):
+    def remove(self, data=None, node=None):
         if node is None:
             x, node = self.search(data)
             if not x:
@@ -91,7 +59,7 @@ class Binary_Search_Tree (object):
         if node.get_right_child():
             n += 1
         father = node.get_father()
-        print('n= ', n)
+        # print('n= ', n)
         if n == 0:
             if father is None:
                 self.root = None
@@ -123,12 +91,102 @@ class Binary_Search_Tree (object):
             while following_number.get_left_child():
                 following_number = following_number.get_left_child()
             node.set_data(following_number.get_data())
-            return self.remove(following_number.get_data(), following_number)
+            return self.remove(node=following_number)
         return None
 
+    def print_tree(self):
+        if self.root is None:
+            print([None])
+            return
+        fathers_node = [self.root]
+        fathers_val = [self.root.get_data()]
+        tree_depth = 0
+        mor_child = True
+        while mor_child:
+            mor_child = False
+            index = 0
+            children_val = []
+            children_node = []
+            for node in fathers_node:
+                if node is None:
+                    children_val += [None, None]
+                    children_node += [None, None]
+                else:
+                    children_node += list(fathers_node[index].get_children())
+                    if children_node[-2] is None:
+                        children_val += [None]
+                    else:
+                        children_val += [children_node[-2].get_data()]
+                    if children_node[-1] is None:
+                        children_val += [None]
+                    else:
+                        children_val += [children_node[-1].get_data()]
+                    mor_child = True
+                index += 1
+            tree_depth += 1
+            if mor_child:
+                print(fathers_val)
+            fathers_val = children_val
+            fathers_node = children_node
 
-def main():
-    pass
+
+class Node (object):
+    def __init__(self, data, father=None, left_child=None, right_child=None):
+        self.data = data
+        self.father = father
+        self.left_child = left_child
+        self.right_child = right_child
+
+    def get_data(self):
+        return self.data
+
+    def get_children(self):
+        return self.left_child, self.right_child
+
+    def get_left_child(self):
+        return self.left_child
+
+    def get_father(self):
+        return self.father
+
+    def get_right_child(self):
+        return self.right_child
+
+    def set_data(self, new_data):
+        self.data = new_data
+
+    def set_father(self, new_father):
+        self.father = new_father
+
+    def set_left_child(self, new_left_child):
+        self.left_child = new_left_child
+
+    def set_right_child(self, new_right_child):
+        self.right_child = new_right_child
+
+
+def main():  # test
+    my_tree = Binary_Search_Tree()
+    print("my tree is:")
+    my_tree.print_tree()
+    my_tree.add(5)
+    my_tree.add(3)
+    my_tree.add(1)
+    my_tree.add(4)
+    my_tree.add(33)
+    my_tree.add(23)
+    my_tree.add(43)
+    my_tree.add(2)
+    my_tree.add(35)
+
+    print("my tree is:")
+    my_tree.print_tree()
+    print("search 33 = ", my_tree.search(33)[0])
+    print("remove 33 = ", my_tree.remove(33))
+    print("remove 33 = ", my_tree.remove(33))
+    print("my tree is:")
+    my_tree.print_tree()
+    print("search 33 = ", my_tree.search(33)[0])
     return 0
 
 
